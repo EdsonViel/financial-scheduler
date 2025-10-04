ed1935102696ba2ce6096c987696643cccc63d53
Edson Viel <viel.edson@gmail.com>
2025-10-04 14:26:28 -0300

    init backend

---
+++ backend/src/test/java/com/example/financial_scheduler/FinancialSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financial_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
