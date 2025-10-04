227fc30b19e2c275a22bffaf232375e71575dd05
Edson Viel <viel.edson@gmail.com>
2025-10-04 17:35:56 -0300

    created RuleException

---
+++ backend/src/main/java/com/example/financial_scheduler/common/exceptions/RuleException.java
@@ -0,0 +1,11 @@
+package com.example.financial_scheduler.common.exceptions;
+
+public class RuleException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+
+    public RuleException(String message) {
+        super(message);
+    }
+
+}
